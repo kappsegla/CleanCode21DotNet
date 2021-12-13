eb7cffba7a671263007b04c0b57ede6e8c5df109
Martin Blomberg <martin.blomberg@outlook.com>
2021-12-13 18:39:48 +0100

    Adds GildedRose

---
+++ GildedRose/Item.cs
@@ -0,0 +1,24 @@
+namespace GildedRose
+{
+    public class Item
+    {
+
+        public string name;
+
+        public int sellIn;
+
+        public int quality;
+
+        public Item(string name, int sellIn, int quality)
+        {
+            this.name = name;
+            this.sellIn = sellIn;
+            this.quality = quality;
+        }
+
+        public override string ToString()
+        {
+            return this.name + ", " + this.sellIn + ", " + this.quality;
+        }
+    }
+}
